beed8f25d96d65ba1be1c2519c2f6c71a195f428
jzaho2000 <jyrki.aho@myy.haaga-helia.fi>
2021-04-18 12:11:48 +0300

    versio 3.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tunkeutumistestaus
+
+Linkki tunkeutumistestaukseen: https://jzaho2000.github.io/Tunkeutumistestaus/
---
+++ js/main.js
@@ -0,0 +1,11 @@
+function showMenu() {
+  var x = document.getElementById("dropDownNav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+	
+
+}
+
